ce27ce14df52d6b7dd93bad647bb994a2fa334ca
Curtain <nickhy@126.com>
2015-09-13 20:57:00 +0800

    添加新模块

---
+++ src/main/java/com/cako/basic/tribune/note/service/impl/UserNoteServiceImpl.java
@@ -0,0 +1,9 @@
+package com.cako.basic.tribune.note.service.impl;
+
+import com.cako.basic.tribune.note.entity.UserNote;
+import com.cako.basic.tribune.note.service.UserNoteService;
+import com.orm.commons.service.impl.DefaulfAbstractService;
+
+public class UserNoteServiceImpl extends DefaulfAbstractService<UserNote, String> implements UserNoteService {
+
+}
---
+++ src/main/java/com/cako/basic/tribune/reply/service/ReplyNoteService.java
@@ -0,0 +1,8 @@
+package com.cako.basic.tribune.reply.service;
+
+import com.cako.basic.tribune.reply.entity.ReplyNote;
+import com.orm.commons.service.BaseService;
+
+public interface ReplyNoteService extends BaseService<ReplyNote, String> {
+
+}
