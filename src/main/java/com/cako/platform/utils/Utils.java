4143ce3ce4690743f9851cf6725d43cfbd27fe21
HUANGYUAN <nickhy@126.com>
2015-10-08 17:49:49 +0630

    删除没有用的变量

--- src/main/java/com/cako/platform/utils/Utils.java
+++ src/main/java/com/cako/platform/utils/Utils.java
@@ -2,7 +2,6 @@ package com.cako.platform.utils;
 
 public class Utils {
 	public static String BLOCK = "__jsp_override__";
-	public static String NEXT_LINE = "\n";
 
 	public static String getOverrideVariableName(String name) {
 		return BLOCK + name;
